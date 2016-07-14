3abde7d445add17be91f84b73820e18ee21484be
arjun1321 <guptaarjun152@gmail.com>
2016-07-14 17:48:59 +0530

    jQuery Draggable

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Script
+
+$(function() {
+   
+    $('.box').draggable();
+    
+    $('#box1').draggable({ scroll:true });
+    
+    $('#box2').draggable({ axis: "x" });
+    $('#box3').draggable({ axis: "y" });
+    
+    $('#box4').draggable({ containment: ".container" });
+});
